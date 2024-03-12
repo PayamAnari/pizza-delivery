cf1b95100967143f53a78a1a3f3fc9fa1334d04b
PayamAnari <anari.p62@gmail.com>
2024-03-12 20:32:04 +0100

    Create superuser func

--- authentication/models.py
+++ authentication/models.py
@@ -16,3 +16,17 @@ class CustomUserManager(BaseUserManager):
         new_user.set_password(password)
         new_user.save()
         return new_user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        if extra_fields.get("is_active") is not True:
+            raise ValueError(_("Superuser must have is_active=True."))
+
+        return self.create_user(email, password, **extra_fields)
