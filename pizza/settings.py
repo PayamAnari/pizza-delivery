1bfa80f293b43a759a5449391d190981f0e6d2af
PayamAnari <anari.p62@gmail.com>
2024-03-12 22:18:18 +0100

    Create migrations

--- pizza/settings.py
+++ pizza/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = "pizza.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pizza-delivery",
+        "USER": "root",
+        "PASSWORD": "2219499",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
