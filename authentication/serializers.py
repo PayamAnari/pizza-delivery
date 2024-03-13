b18ac8739443f91d59fa9371a8aeb334a7e4f2bb
PayamAnari <anari.p62@gmail.com>
2024-03-13 16:21:58 +0100

    Modify serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,19 +4,25 @@ from phonenumber_field.serializerfields import PhoneNumberField
 
 
 class UserCreationSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(("first name"), max_length=30, allow_blank=True)
-    last_name = serializers.CharField(("last name"), max_length=30, allow_blank=False)
-    username = serializers.CharField(("username"), max_length=40, allow_blank=False)
-    email = serializers.EmailField(("email address"), max_length=80, unique=True)
+    first_name = serializers.CharField(
+        label=("first name"), max_length=30, allow_blank=True
+    )
+    last_name = serializers.CharField(
+        label=("last name"), max_length=30, allow_blank=False
+    )
+    username = serializers.CharField(
+        label=("username"), max_length=40, allow_blank=False
+    )
+    email = serializers.EmailField(label=("email address"), max_length=80)
     phone_number = PhoneNumberField(
-        ("phone number"), allow_blank=False, allow_null=False
+        label=("phone number"), allow_blank=False, allow_null=False
     )
     password = serializers.CharField(
-        ("password"), min_length=8, allow_blank=False, write_only=True
+        label=("password"), min_length=8, allow_blank=False, write_only=True
     )
-    date_of_birth = serializers.DateField(("date of birth"), allow_null=True)
+    date_of_birth = serializers.DateField(label=("date of birth"), allow_null=True)
     delivery_address = serializers.CharField(
-        ("delivery_address"), max_length=255, allow_blank=True, allow_null=True
+        label=("delivery_address"), max_length=255, allow_blank=True, allow_null=True
     )
 
     class Meta:
@@ -44,9 +50,14 @@ class UserCreationSerializer(serializers.ModelSerializer):
         if email:
             raise serializers.validationError(detail="User with email exists")
 
-        phone_number = User.objects.filter(username=attrs["phone_number"]).exists()
+        phonenumber = User.objects.filter(username=attrs["phone_number"]).exists()
 
-        if phone_number:
+        if phonenumber:
             raise serializers.validationError(detail="User with phone number exists")
+
+        address = User.objects.filter(username=attrs["delivery_address"]).exists()
+
+        if address:
+            raise serializers.validationError(detail="User with address exists")
 
         return super().validate(attrs)
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from .models import User
+from . import serializers
 
 
 # Create your views here.
