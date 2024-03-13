f219bca7a8c5a550291fc068370773dbc9296615
PayamAnari <anari.p62@gmail.com>
2024-03-13 11:09:08 +0100

    Modify order model

--- orders/models.py
+++ orders/models.py
@@ -18,13 +18,19 @@ class Order(models.Model):
     ORDER_STATUS = (
         ("PENDING", "pending"),
         ("IN_TRANSIT", "inTransit"),
-        ("DELIVERD", "deliver"),
+        ("DELIVERED", "delivered"),
         ("CANCELLED", "cancelled"),
     )
 
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     size = models.CharField(max_length=20, choices=SIZES, default=SIZES[0][0])
-    orde_status = models.CharField(
-        max_length=20, chiocese=ORDER_STATUS, default=ORDER_STATUS[0][0]
+    order_status = models.CharField(
+        max_length=20, choices=ORDER_STATUS, default=ORDER_STATUS[0][0]
     )
     quantity = models.IntegerField()
+    created_at = models.DatetimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+def __str__(self):
+    return f"<Order {self.size} by {self.customer.id}"
