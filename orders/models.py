bdcced431c014be2eb331a9c6883d4a646e77aa5
PayamAnari <anari.p62@gmail.com>
2024-03-13 10:50:39 +0100

    Modify order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Order(models.Model):
+
+    SIZES = (
+        ("SMALL", "small"),
+        ("MEDIUM", "medium"),
+        ("LARGE", "large"),
+        ("EXTRA_LARGE", "extra_large"),
+    )
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=20)
