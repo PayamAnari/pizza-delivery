755dc1a900618adce6d69fab1c59c448b5f75a45
PayamAnari <anari.p62@gmail.com>
2024-03-14 20:51:55 +0100

    Modify access token lifetime

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,11 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("Bearer",)}
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 
 MIDDLEWARE = [
