915f5dbbc41ad109381f24baa72fc2df839ef87c
PayamAnari <anari.p62@gmail.com>
2024-03-14 21:53:54 +0100

    Add views order

--- orders/views.py
+++ orders/views.py
@@ -8,10 +8,23 @@ class OrderCreateListView(generics.GenericAPIView):
     def get(self, request):
         pass
 
-    
     def post(self, request):
         pass
 
 
-
 class OrderDetailView(generics.GenericAPIView):
+
+    def get(self, request, order_id):
+        pass
+
+    def post(self, request, order_id):
+        pass
+
+
+class OrderDetailView(generics.GeneriAPIView):
+
+    def get(self, request, order_id):
+        pass
+
+    def put(self, request, order_id):
+        pass
