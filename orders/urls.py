edb5b9025eb27216000696189e0807c9d43be2d1
PayamAnari <anari.p62@gmail.com>
2024-03-15 11:43:07 +0100

    Add order by id path

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("orders/", views.OrderCreateListView.as_view(), name="orders"),
+    path(
+        "orders/<int:order_id>/", views.OrderDetailView.as_view(), name="order_detail"
+    ),
 ]
