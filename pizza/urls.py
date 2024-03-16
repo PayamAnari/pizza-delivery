1f0b3a8c81964d55e967e4781b39d461a1ce87dd
PayamAnari <anari.p62@gmail.com>
2024-03-16 15:08:45 +0100

    Add swagger

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Pizza Delivery API",
+        default_version="v1",
+        description="This is a REST API for a Pizza Delivery service",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
